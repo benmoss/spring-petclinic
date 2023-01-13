444ff250b9404c693939ade2643d457904b8a757
Ben Moss <ben@mossity.com>
2023-01-13 14:28:32 -0500

    Merge steps

--- .buildkite/steps/cleanup.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-set -euxo pipefail
-
-kubectl delete ns petclinic --wait --timeout=90s --ignore-not-found
--- .buildkite/steps/integration-tests.sh
+++ .buildkite/steps/integration-tests.sh
@@ -7,3 +7,10 @@ image=$(buildkite-agent meta-data get "image")
 kubectl create ns petclinic --dry-run -oyaml | kubectl apply -f-
 sed "s|ttl.sh/bmo:1h|$image|" manifest.yml | kubectl apply -n petclinic -f-
 kubectl rollout status deployment petclinic -n petclinic --timeout=90s
+
+function cleanup {
+    kubectl delete ns petclinic --wait --timeout=90s --ignore-not-found
+}
+trap cleanup EXIT
+
+curl --no-progress-meter petclinic.petclinic
