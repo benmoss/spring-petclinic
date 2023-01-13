043aa07e0338349bc7c0fc8a01cafa269ec934e7
Ben Moss <ben@mossity.com>
2023-01-13 10:01:04 -0500

    Run tests, emit test results

--- .buildkite/steps/build.sh
+++ .buildkite/steps/build.sh
@@ -2,7 +2,7 @@
 
 set -euxo pipefail
 
-./gradlew jib --image ttl.sh/spring-petclinic:1h
+./gradlew test jib --image ttl.sh/spring-petclinic:1h
 
 digest=$(cat ./build/jib-image.digest)
 buildkite-agent meta-data set "image" "ttl.sh/spring-petclinic@${digest}"
