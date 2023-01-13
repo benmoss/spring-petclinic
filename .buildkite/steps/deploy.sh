240b361e63248aa4af3844e14c13eaa51f07afa0
Ben Moss <ben@mossity.com>
2023-01-13 09:49:28 -0500

    fix sed

--- .buildkite/steps/deploy.sh
+++ .buildkite/steps/deploy.sh
@@ -5,5 +5,5 @@ set -euxo pipefail
 image=$(buildkite-agent meta-data get "image")
 
 kubectl create ns petclinic --dry-run -oyaml | kubectl apply -f-
-sed "s|ttl.sh/bmo:1h|$image" manifest.yml | kubectl apply -n petclinic -f-
+sed "s|ttl.sh/bmo:1h|$image|" manifest.yml | kubectl apply -n petclinic -f-
 kubectl rollout status deployment petclinic -n petclinic --timeout=90s
